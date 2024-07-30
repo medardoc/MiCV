b28d57dac06f86753261328f3db2ea4f7e9a3c70
medardoc <m.carpio2@alumnos.santotomas.cl>
2024-07-29 21:35:03 -0400

    algunas mejoras

--- JS/script.js
+++ JS/script.js
@@ -17,32 +17,3 @@ function showSlides() {
     slides[slideIndex - 1].style.display = 'block';
     setTimeout(showSlides, 3000); // Cambiar imagen cada 3 segundos
 }
-
-// Inicializar EmailJS con tu User ID
-(function(){
-    emailjs.init('service_u2ucdmo'); // Reemplaza 'YOUR_USER_ID' con tu User ID de EmailJS
-})();
-
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    const templateParams = {
-        from_name: name,
-        from_email: email,
-        message: message
-    };
-
-    emailjs.send('service_u2ucdmo', 'service_u2ucdmo', templateParams) // Reemplaza 'YOUR_TEMPLATE_ID' con el ID de tu plantilla
-        .then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-            alert('Mensaje enviado exitosamente');
-            window.location.href = 'http://localhost:3000'; // Redirige al panel
-        }, function(error) {
-            console.log('FAILED...', error);
-            alert('Error al enviar el mensaje');
-        });
-});
